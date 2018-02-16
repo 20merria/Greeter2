58655542278ec95e8d53e3c27e638cf9c863938c
Unknown <20merria@elmbrookstudents.org>
2018-02-16 12:04:56 -0600

    Merge branch 'Greeter' into dev

---
+++ src/TestGreeter.java
@@ -0,0 +1,14 @@
+
+public class TestGreeter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeter bob = new Greeter();
+		bob.toString();
+		Greeter[]many = new Greeter[5];
+		for(Greeter i:many) {
+			i.toString();
+		}
+	}
+
+}
